3bde838f2c9d94a5bdb0831459849e61247a707c
Gayle Hoefer <gaylehoefer@gmail.com>
2022-10-26 19:57:46 -0700

    added handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,12 @@
+// WHEN I click on any other links in the navigation
+// THEN I am prompted to either sign up or sign in
+// WHEN I choose to sign up
+// THEN I am prompted to create a username and password
+// WHEN I click on the sign-up button
+//THEN my user credentials are saved and I am logged into the site
+// WHEN I revisit the site at a later time and choose to sign in
+//THEN I am prompted to enter my username and password
+
 const loginFormHandler = async (event) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     event.preventDefault();
