b6bb321bb9fd0d43a6e5e18ed533b799aea7697f
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-11-02 08:30:26 -0700

    finished model index.js folder

--- models/index.js
+++ models/index.js
@@ -2,19 +2,33 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
 
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
+
 Post.belongsTo(User,{
     foreignKey: "user_id",
     onDelete: "CASCADE"
-})
+});
 
 Post.hasMany(Comment,{
     foreignKey: "post_id",
     onDelete: "CASCADE"
-})
+});
 
 Comment.belongsTo(User, {
     foreignKey: "user_id",
     onDelete: "CASCADE"
-})
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "Post_id",
+    onDelete: "CASCADE"
+});
+
+User.hasMany(comment, {
+    foreignKey: "user_id",
+    onDelete: "cascade"
+});
 
 module.exports = { User, Comment, Post };
