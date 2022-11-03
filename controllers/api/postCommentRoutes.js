96cda720d523ea7e6af4f94e21478b178421e4a7
Gayle Hoefer <gaylehoefer@gmail.com>
2022-11-03 00:49:19 -0700

    commentRoutes done but with errors

--- controllers/api/postCommentRoutes.js
+++ controllers/api/postCommentRoutes.js
@@ -1,2 +1,76 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Comment.findAll({});
+        return res.json(postData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Comment.findAll({
+            where: {
+                id: req.params.id,
+            }
+        });
+        return res.json(postData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Comment.create({
+            where: {
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id,
+            }
+        });
+        return res.json(postData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// router.get('/:id', withAuth async (req, res) => {
+//     try {
+//         const postData = await Comment.update(
+//             {
+//                 where: {
+//                     id: req.params.id,
+//                 },
+//                 {
+//                 comment_text: req.body.comment_text,
+//             }   
+//             return res.json(postData)
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Comment.destroy(
+            {
+                where: {
+                    id: req.params.id
+                }
+            })
+        if (!postData) {
+            res.status(404).json({ message: "No post found" })
+        }
+        return res.json(postData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -101,23 +101,23 @@ router.get('/', async (req, res) => {
 //     }
 // });
 
-router.delete('/:id', withAuth async (req, res) => {
-    try {
-        const postData = await Post.destroy(
-            {
-                where: {
-                    id: req.params.id
-                }
-            },
+// router.delete('/:id', withAuth async (req, res) => {
+//     try {
+//         const postData = await Post.destroy(
+//             {
+//                 where: {
+//                     id: req.params.id
+//                 }
+//             },
 
-                if (!postData) {
-            res.status(404).json({ message: "No post found" })
-        }
-        return res.json(postData)
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+//                 if (!postData) {
+//             res.status(404).json({ message: "No post found" })
+//         }
+//         return res.json(postData)
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 
 module.exports = router;
