8fdbab8d83910ab91443a0e3dc5c7b156647179e
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-11-06 12:43:00 -0700

    added db

---
+++ db/insert-select.sql
@@ -0,0 +1,14 @@
+INSERT INTO biographies (id, name)
+VALUES
+{
+        title: "test 1",
+        post_content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        user_id: 1
+    },
+    {
+        title: "test 2",
+        post_content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        user_id: 2
+    },
+]
+SELECT * FROM biographies;
